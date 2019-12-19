852708d61c8af6e73283973558b31b0b41880d43
Manuel Carrasco <mcarrasco@dc.uba.ar>
2019-12-18 22:02:23 -0300

    Added poc

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using FastHashes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me something to hash:");
+            string input = Console.ReadLine();
+            var bytes = Encoding.Default.GetBytes(input);
+            var hash = new MurmurHash32();
+            var hashBytes = hash.ComputeHash(bytes);
+            Console.WriteLine(Encoding.Default.GetString(hashBytes));
+        }
+    }
+}
